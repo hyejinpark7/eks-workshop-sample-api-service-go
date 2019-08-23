6c98fb863e505e1068703bc1aed3c39eeffda00d
hyejinpark7 <h713.park@samsung.com>
2019-08-23 14:08:13 +0900

    Change the message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World eksworkshop"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
